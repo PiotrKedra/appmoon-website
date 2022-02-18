5367f37cd716a3f2bd8aeccf649a1d65776665c5
piotrkedra <piotr.k9750@gmail.com>
2022-02-18 18:02:10 +0100

    test font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,5 @@
-import "@fontsource/jetbrains-mono"
+import "@fontsource/jetbrains-mono/700.css"
+import "@fontsource/jetbrains-mono/200.css"
+import "@fontsource/jetbrains-mono/300.css"
+import "@fontsource/jetbrains-mono/400.css"
+import "@fontsource/vt323"
