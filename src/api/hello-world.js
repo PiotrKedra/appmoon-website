2f27bfc9828e866125e48aed13231f07dd0f6c77
piotrkedra <piotr.k9750@gmail.com>
2023-11-05 22:13:41 +0100

    test netlify serverless sendgrid emails

--- src/api/hello-world.js
+++ src/api/hello-world.js
@@ -1,3 +1,22 @@
 export default function handler(req, res) {
-  res.status(200).json({ hello: `world` })
+
+  const sgMail = require('@sendgrid/mail')
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    to: 'pkedra@codenauts.io', // Change to your recipient
+    from: 'contact@codenauts.io', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: 'Bobik <strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+      res.status(200).json({ hello: `world` })
+    })
+    .catch((error) => {
+      console.error(error)
+      res.status(200).json({ hello: `sth not working` })
+    })
 }
